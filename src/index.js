b62403c5c54e6327793873dc244b204510a3ad4d
Guillaume Grossetie <ggrossetie@gmail.com>
2018-11-26 00:00:49 +0100

    Initialize project

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const twemojiMap = require('./twemoji-map')
+
+function emojiInlineMacro () {
+  const self = this
+  self.named('emoji')
+  self.positionalAttributes('size')
+
+  const defaultSize = '24px'
+  const sizeMap = {'1x': '17px', 'lg': defaultSize, '2x': '34px', '3x': '50px', '4x': '68px', '5x': '85px'}
+
+  self.process(function (parent, target, attrs) {
+    const sizeAttr = attrs.size
+    let size
+    if (sizeAttr && sizeAttr.trim().endsWith('px')) {
+      size = sizeAttr
+    } else if (sizeAttr && sizeMap[sizeAttr]) {
+      size = sizeMap[sizeAttr]
+    } else {
+      size = defaultSize
+    }
+    const emojiUnicode = twemojiMap[target]
+    if (emojiUnicode) {
+      return `<img class="emoji" draggable="false" height="${size}" width="${size}" src="https://twemoji.maxcdn.com/2/svg/${emojiUnicode}.svg" />`
+    }
+    console.warn(`Skipping emoji inline macro. ${target} not found`)
+    return `[emoji ${target} not found]`;
+  })
+}
+
+module.exports.register = function register (registry) {
+  if (typeof registry.register === 'function') {
+    registry.register(function () {
+      this.inlineMacro(emojiInlineMacro)
+    })
+  } else if (typeof registry.block === 'function') {
+    registry.inlineMacro(emojiInlineMacro)
+  }
+  return registry
+}
---
+++ test/test.js
@@ -0,0 +1,68 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const asciidoctorEmoji = require('../src/index.js')
+const asciidoctor = require('asciidoctor.js')()
+
+describe('Registration', () => {
+  it('should register the extension', () => {
+    const registry = asciidoctor.Extensions.create()
+    expect(registry['$inline_macros?']()).to.be.false
+    asciidoctorEmoji.register(registry)
+    expect(registry['$inline_macros?']()).to.be.true
+    expect(registry['$registered_for_inline_macro?']('emoji')).to.be.an('object')
+  })
+})
+
+describe('Conversion', () => {
+  describe('When extension is not registered', () => {
+    it('should not convert an existing emoji', () => {
+      const input = `emoji:smile[]`
+      const html = asciidoctor.convert(input)
+      expect(html).to.contain('emoji:smile[]')
+    })
+    it('should not convert an non existing emoji', () => {
+      const input = `emoji:ooops[]`
+      const html = asciidoctor.convert(input)
+      expect(html).to.contain('emoji:ooops[]')
+    })
+  })
+  describe('When extension is registered', () => {
+    it('should convert an existing emoji into an image', () => {
+      const input = `emoji:smile[]`
+      const registry = asciidoctor.Extensions.create()
+      asciidoctorEmoji.register(registry)
+      const html = asciidoctor.convert(input, {extension_registry: registry})
+      expect(html).to.contain('<img class="emoji" draggable="false" height="24px" width="24px" src="https://twemoji.maxcdn.com/2/svg/1f604.svg" />')
+    })
+    it('should return an error message if the emoji does not exist', () => {
+      const input = `emoji:ooops[]`
+      const registry = asciidoctor.Extensions.create()
+      asciidoctorEmoji.register(registry)
+      const html = asciidoctor.convert(input, {extension_registry: registry})
+      expect(html).to.contain('[emoji ooops not found]')
+    })
+  })
+  it('should convert an existing emoji into an image with the size 2x (34px)', () => {
+    const input = `emoji:santa-skin-tone-6[2x]`
+    const registry = asciidoctor.Extensions.create()
+    asciidoctorEmoji.register(registry)
+    const html = asciidoctor.convert(input, {extension_registry: registry})
+    expect(html).to.contain('<img class="emoji" draggable="false" height="34px" width="34px" src="https://twemoji.maxcdn.com/2/svg/1f385-1f3ff.svg" />')
+  })
+  it('should convert an existing emoji into an image with the size 4x (68px)', () => {
+    const input = `emoji:beetle[4x]`
+    const registry = asciidoctor.Extensions.create()
+    asciidoctorEmoji.register(registry)
+    const html = asciidoctor.convert(input, {extension_registry: registry})
+    expect(html).to.contain('<img class="emoji" draggable="false" height="68px" width="68px" src="https://twemoji.maxcdn.com/2/svg/1f41e.svg" />')
+  })
+  it('should convert an existing emoji into an image with the size in pixel (42px)', () => {
+    const input = `emoji:penguin[42px]`
+    const registry = asciidoctor.Extensions.create()
+    asciidoctorEmoji.register(registry)
+    const html = asciidoctor.convert(input, {extension_registry: registry})
+    expect(html).to.contain('<img class="emoji" draggable="false" height="42px" width="42px" src="https://twemoji.maxcdn.com/2/svg/1f427.svg" />')
+  })
+})
+
